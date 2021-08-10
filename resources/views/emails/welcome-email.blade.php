58a5179c630f9cefd2ab376ff1f17018f5aad748
mnboy19 <jchrisboy0@gmail.com>
2021-08-10 12:35:41 -0500

    first commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to freeCodeGram
+
+This is a community of fellow developpers and we love that you joined us.
+
+All the best,<br>
+jean-christian katungulu
+@endcomponent
